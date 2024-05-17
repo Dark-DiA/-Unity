5ed9007a3f64eb34985222cc644c53f2953a2c10
Dark-DiA <133471175+Dark-DiA@users.noreply.github.com>
2024-05-17 13:29:47 +1100

    Add files via upload

---
+++ m56/Patterns_Part2.Final/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+using YoutubeExplode;
+using YoutubeExplode.Videos;
+
+namespace m56.Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ссылку на Youtube-видео:");
+            
+            string url = Console.ReadLine() ?? "";
+            //"https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+
+            YoutubeClient youtubeClient = new YoutubeClient();
+            VideoId? videoId= new VideoId();
+
+            videoId = VideoId.TryParse(url);
+
+            if (videoId == null) 
+            {
+                ///Проверяем
+                ///может это YouTube Shorts 
+                ///т.к. TryParse не находит его
+
+                videoId = Regex.Match(url, "youtube\\..+?\\/shorts\\/(.*?)(?:\\?|&|\\/|$)").Groups[1].Value;
+                if (!string.IsNullOrWhiteSpace(videoId))
+                    goto VideoExists;
+
+                Console.WriteLine("Видео не найдено.");
+                return;
+            }
+VideoExists:
+
+            var sender = new Sender();
+            
+            var receiver = new Receiver();
+
+            var videoInfo = new CommandVideoInfo(receiver, youtubeClient);
+            sender.SetCommand(videoInfo);
+            sender.Execute(videoId);
+
+            var downloadVideo = new CommandDownloadVideo(receiver, youtubeClient);
+            sender.SetCommand(downloadVideo);
+            sender.Execute(videoId);  
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ m56/Patterns_Part2.Final/Receiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m56.Final
+{
+    public class Receiver
+    {
+        public void Operation(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
