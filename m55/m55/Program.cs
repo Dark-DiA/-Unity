ede107824c30fadce5b1cc394506cba9a64d8324
Dark-DiA <133471175+Dark-DiA@users.noreply.github.com>
2024-05-17 12:16:38 +1100

    Add files via upload

---
+++ m55/m55/Program.cs
@@ -0,0 +1,13 @@
+﻿using m55.models;
+
+// Обычный аккаунт 
+BaseAccount baseAccount = new BaseAccount() { Balance = 110000 };
+Calculator.CalculateInterest(baseAccount);
+
+
+// Зарплатный аккаунт
+SalaryAccount salaryAccount = new SalaryAccount() { Balance = 110000 };
+Calculator.CalculateInterest(salaryAccount);
+
+
+Console.ReadLine();
---
+++ m55/m55/models/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using m55.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m55.models
+{
+    internal static class Calculator
+    {
+        // Метод для расчета процентной ставки
+        public static void CalculateInterest(ICalculateInterest account)
+        {
+            account.CalculateInterest();
+        }
+    }
+}
